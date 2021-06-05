84104ac4eb3a92e08de8ca353b2fe79a2fb7e7d1
renshimosawa <polaris2760@gmail.com>
2021-06-05 10:09:18 +0900

    fix

--- components/domains/HeadCompo/index.tsx
+++ components/domains/HeadCompo/index.tsx
@@ -8,12 +8,18 @@ const HeadCompo: React.FC = () => {
       <title>Emotional Aomori</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="description" content="下沢廉のポートフォリオです。Next.jsで作りました。" />
+      <meta
+        property="description"
+        content="下沢廉のポートフォリオサイトです。Next.jsで作りました。"
+      />
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
       <meta property="og:url" content="https://emotional-aomori.com" />
       <meta property="og:title" content="Emotional Aomori" />
-      <meta property="og:description" content="下沢廉のポートフォリオです。Next.jsで作りました。" />
+      <meta
+        property="og:description"
+        content="下沢廉のポートフォリオサイトです。Next.jsで作りました。"
+      />
       <meta
         property="og:image"
         content="https://firebasestorage.googleapis.com/v0/b/emotional-aomori.appspot.com/o/OGP.png?alt=media&token=fe348853-d2cd-4b08-8906-33eb47b3664f"
@@ -25,7 +31,7 @@ const HeadCompo: React.FC = () => {
       <meta property="twitter:title" content="Emotional Aomori" />
       <meta
         property="twitter:description"
-        content="下沢廉のポートフォリオです。Next.jsで作りました。"
+        content="下沢廉のポートフォリオサイトです。Next.jsで作りました。"
       />
       <meta
         property="twitter:image"
