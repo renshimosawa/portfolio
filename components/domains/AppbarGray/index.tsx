ef5a74f8e01fa65cd4f65d0a77b692c2ffb38ee1
renshimosawa <polaris2760@gmail.com>
2021-06-10 09:04:15 +0900

    fixed

--- components/domains/AppbarGray/index.tsx
+++ components/domains/AppbarGray/index.tsx
@@ -1,6 +1,5 @@
 import styles from './AppbarGray.module.scss'
 import cn from 'classnames'
-import Image from 'next/image'
 
 export type Props = {
   className?: string
@@ -10,7 +9,7 @@ export type Props = {
 const AppbarGray: React.FC<Props> = ({ onClick, className }) => (
   <div className={cn(styles.default, className)}>
     <button onClick={onClick} className={styles.button}>
-      <Image className={styles.logo} src="/logo_gray.svg" width={250} height={50} />
+      <img className={styles.logo} src="/logo_gray.svg" width={250} height={50} />
     </button>
   </div>
 )
