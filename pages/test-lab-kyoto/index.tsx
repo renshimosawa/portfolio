4551713417e1204477bdf8ad3bab7203d8afd329
Ren Shimosawa <48125368+renshimosawa@users.noreply.github.com>
2022-10-21 23:39:02 +0900

    Revert "Develop"

--- pages/test-lab-kyoto/index.tsx
+++ pages/test-lab-kyoto/index.tsx
@@ -28,6 +28,9 @@ const testLabKyoto: React.FC = () => {
         twitterImage={OgpImage}
       />
       <AppbarGray onClick={() => router.push('/')} />
+      <h2 style={{ padding: '0 20px', textAlign: 'center' }}>
+        文久改正八戸御城下略図 on Google Map
+      </h2>
       <OverlayMapKyoto />
     </div>
   )
