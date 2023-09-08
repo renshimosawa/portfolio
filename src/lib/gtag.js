e93b8043beac8062c9da30cdbc8682ababab96f1
renshimosawa <polaris2760@gmail.com>
2023-09-08 14:10:44 +0900

    fix env

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID
+export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
 
 export const pageview = (url) => {
   window.gtag('config', GA_MEASUREMENT_ID, {
